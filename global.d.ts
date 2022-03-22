ec75b822029ebada90e090cb1ffc0afafd5b5886
Getulio Strapazzon <strapazzon09@gmail.com>
2022-03-22 18:29:21 +0000

    feat(declare module): declare global module wipe.css

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+/*
+This is a Next.js project bootstrapped with Strapazzon/next-starter
+*/
+
+declare module 'wipe.css'
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,5 @@
-/*
-This is a Next.js project bootstrapped with Strapazzon/next-starter
-*/
-
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
